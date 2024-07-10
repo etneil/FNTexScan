50be4f1ff8ae4d2522284e39a3869f1cf4e99513
Ethan Neil <ethan.neil@colorado.edu>
2024-07-10 15:54:31 -0600

    Added script to study top-20 textures.

---
+++ FNTexScan/generate_studies.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+./FNTexScan study 3,2,-4,-2,-3,-3,-3 report_1 --thy_per_tex=1000 --opt_SM
+./FNTexScan study 3,2,-4,-2,-4,-3,-3 report_2 --thy_per_tex=1000 --opt_SM
+./FNTexScan study 3,2,-3,-1,-3,-2,-2 report_3 --thy_per_tex=1000 --opt_SM
+./FNTexScan study 3,2,-4,-1,-3,-3,-3 report_4 --thy_per_tex=1000 --opt_SM
+./FNTexScan study 4,3,-4,-2,-4,-3,-3 report_5 --thy_per_tex=1000 --opt_SM
+./FNTexScan study 3,2,-4,-1,-3,-3,-2 report_6 --thy_per_tex=1000 --opt_SM
+./FNTexScan study 4,2,-4,-2,-4,-3,-3 report_7 --thy_per_tex=1000 --opt_SM
+./FNTexScan study 3,2,-3,-1,-2,-2,-2 report_8 --thy_per_tex=1000 --opt_SM
+./FNTexScan study 3,2,-3,-1,-3,-3,-2 report_9 --thy_per_tex=1000 --opt_SM
+./FNTexScan study 3,2,-4,-2,-3,-3,-2 report_10 --thy_per_tex=1000 --opt_SM
+./FNTexScan study 2,1,-3,-1,-2,-2,-2 report_11 --thy_per_tex=1000 --opt_SM
+./FNTexScan study 4,3,-4,-1,-4,-3,-3 report_12 --thy_per_tex=1000 --opt_SM
+./FNTexScan study 4,3,-4,-2,-4,-4,-3 report_13 --thy_per_tex=1000 --opt_SM
+./FNTexScan study 3,2,-4,-2,-4,-3,-2 report_14 --thy_per_tex=1000 --opt_SM
+./FNTexScan study 4,3,-4,-2,-3,-3,-3 report_15 --thy_per_tex=1000 --opt_SM
+./FNTexScan study 4,3,-4,-1,-3,-3,-3 report_16 --thy_per_tex=1000 --opt_SM
+./FNTexScan study 4,2,-4,-2,-4,-4,-3 report_17 --thy_per_tex=1000 --opt_SM
+./FNTexScan study 4,2,-4,-2,-3,-3,-3 report_18 --thy_per_tex=1000 --opt_SM
+./FNTexScan study 3,2,-4,-1,-3,-2,-2 report_19 --thy_per_tex=1000 --opt_SM
+./FNTexScan study 3,2,-3,-2,-3,-3,-2 report_20 --thy_per_tex=1000 --opt_SM
